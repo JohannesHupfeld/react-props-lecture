492d25f30771cc28a2d0149e0e02052e93c15a9a
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-15 16:11:09 -0500

    notes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import Monster from './Monster'
 
 function App() {
-  const monsters = [{monster:"Wolfman", scare:"Silver"}, {monster:"The Thing", scare:"Fire"}];
+  const monsters = [{monster:"Wolfman", scare:"Silver"}, {monster:"The Thing", scare:"Fire"}, {monster:"Mummy", scare:"Brendan Frasier"}]; // this is like the info you would get from an api i.e Recipe is the object and the keys would be name, ingredients, instructions, image_url.
   const monsterElements = monsters.map((m) => {return <Monster monster={m.monster} scare={m.scare}/>})
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* <Monster /> */}
         {monsterElements}
         {/* <Monster monster="Wolfman" scare="Silver"/>
         <Monster monster="Ghost" scare="people"/>
         <Monster monster="The Thing" scare="Fire"/>
-        <Monster monster="Dracula" scare="Garlic"/> */}
-        {new Monster({monster:"Mummy", scare:"Brendan Fraiser"}).render()}
+        <Monster monster="Dracula" scare="Garlic"/>  written in JSX*/}
+        {/* {Monster({monster:"Mummy", scare:"Brendan Frasier"})} how it would be in JS*/}
+        {/* {new Monster({monster:"Mummy", scare:"Brendan Fraiser"}).render()} Plain JS code -- do not do it this way */}
         {/* key: value */}
         {/* key = {value} */}
         <p>
--- src/Monster.js
+++ src/Monster.js
@@ -2,6 +2,13 @@
 //Want to make sure it is in a H3 
 import React from 'react'
 import Scare from './Scare'
+
+// function Monster() {  
+//     return(
+//         <h3>Wolfman is out tonight </h3>
+//     )
+// }
+
 // export default function Monster(props){
 //    return(
 //        <div>
@@ -12,20 +19,21 @@ import Scare from './Scare'
 // }
 
 // export default Monster
-class Monster extends React.Component{
-// constructor(props){
-// super(props) //this.props = props
-// }
 
-render(){
-    return(
-        <div>
-            <h3>{this.props.monster} is out tonight!</h3>
-            {/* <p>{this.props.scare}</p> */}
-            <Scare scare={this.props.scare}/>
-        </div>
-    )
-}
+class Monster extends React.Component{ // class component
+// constructor(props){
+// super(props) //same as this.props = props
+// }   ****** new versions of react do not even need constructor but its important to know that this is what is going on behind the scenes
+
+    render(){ // since its not static its meant to be run on individual monster instances --new monsters 
+        return(
+            <div>
+                <h3>{this.props.monster} is out tonight!</h3>
+                {/* <p>{this.props.scare}</p> */}
+                <Scare scare={this.props.scare}/>
+            </div>
+        )
+    }
 }
 
 export default Monster
